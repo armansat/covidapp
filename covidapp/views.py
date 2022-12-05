ded9b6d30c8a695fc35fb3a6afd169473d9c7007
armansat <armansat@gmail.com>
2022-12-05 23:47:16 +0600

    new files

---
+++ covidapp/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+import requests
+import json
+import random 
+
+url = "https://covid-193.p.rapidapi.com/statistics"
+
+headers = {
+	"X-RapidAPI-Key": "86c0354dd3msh349881783983608p1180bajsn65fce824e300",
+	"X-RapidAPI-Host": "covid-193.p.rapidapi.com"
+}
+
+response = requests.request("GET", url, headers=headers).json()
+
+
+# Create your views here.
+def covidworldview(request):
+    context = {'response': response['response'][random.randrange(1, 200)]}
+    return render(request,'covidworld.html',context)
